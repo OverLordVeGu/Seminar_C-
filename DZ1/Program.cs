3e64a0f9ea4bd22264eb9595394f007e012bd2e6
VeGu <VeGu@bk.ru>
2023-07-29 19:18:13 +0700

    Домашнее задание с выводом max и min

---
+++ DZ1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите число a: ");
+string NumberA = Console.ReadLine ();
+int a = int.Parse (NumberA);
+Console.Write("Введите число b: ");
+string NumberB = Console.ReadLine ();
+int b = int.Parse (NumberB);
+if (a > b) {
+    Console.Write ("max ");
+    Console.WriteLine (a);
+    Console.Write ("min ");
+    Console.WriteLine (b);
+}
+else {
+    Console.Write ("max ");
+    Console.WriteLine (b);
+    Console.Write ("min ");
+    Console.WriteLine (a); 
+}
