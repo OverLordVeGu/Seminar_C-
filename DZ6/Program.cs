8a76752493d061d307c296f54de016458ca15c19
VeGu <VeGu@bk.ru>
2023-07-31 22:20:02 +0700

    Задача с вводом числа и показом ее третьей цифры

--- DZ6/Program.cs
+++ DZ6/Program.cs
@@ -2,10 +2,32 @@
 Console.Write("Введите число: ");
 string NumberA = Console.ReadLine();
 int num = int.Parse(NumberA);
-int result = (num%10)%10-num;
+int result = 0;
 if (num < 100) {
     Console.WriteLine("Третьей цифры нет");
 }
-else {
+else if (num>99 && num<1000)
+{
+    result = num%10;
+    Console.WriteLine ($"Третья цифра {result}");
+}
+else if (num>999 && num<10000)
+{
+    result = num%100/10;
+    Console.WriteLine ($"Третья цифра {result}");
+}
+else if (num>9999 && num<100000)
+{
+    result = num%1000/100;
+    Console.WriteLine ($"Третья цифра {result}");
+}
+else if (num>99999 && num<1000000)
+{
+    result = num%10000/1000;
+    Console.WriteLine ($"Третья цифра {result}");
+}
+else if (num>999999 && num<10000000)
+{
+    result = num%100000/10000;
     Console.WriteLine ($"Третья цифра {result}");
 }
