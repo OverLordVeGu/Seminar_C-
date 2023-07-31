15d786f3967ebfa1b97dbde760d34bb973d75848
VeGu <VeGu@bk.ru>
2023-07-31 21:42:18 +0700

    Домашнее задание с днями недели

---
+++ DZ6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string NumberA = Console.ReadLine();
+int num = int.Parse(NumberA);
+int result = (num%10)%10-num;
+if (num < 100) {
+    Console.WriteLine("Третьей цифры нет");
+}
+else {
+    Console.WriteLine ($"Третья цифра {result}");
+}
---
+++ DZ7/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Clear ();
+Console.Write ("Введите номер дня: ");
+string dayNum = Console.ReadLine ();
+int numday = int.Parse (dayNum);
+string day = string.Empty;
+
+if (numday == 1){
+    day = "Понедельник";
+    Console.WriteLine ($"Вы ввели день {day}");
+} 
+else if (numday == 2) {
+    day = "Вторник";
+    Console.WriteLine ($"Вы ввели день {day}");
+}
+else if (numday == 3) {
+    day = "Среда";
+    Console.WriteLine ($"Вы ввели день {day}");
+}
+else if (numday == 4) {
+    day = "Четверг";
+    Console.WriteLine ($"Вы ввели день {day}");
+}
+else if (numday == 5) {
+    day = "Пятница";
+    Console.WriteLine ($"Вы ввели день {day}");
+}
+else if (numday == 6) {
+    day = "Суббота";
+    Console.WriteLine ($"Вы ввели день {day}");
+}
+else if (numday == 7) {
+    day = "Воскресенье";
+    Console.WriteLine ($"Вы ввели день {day}");
+}
+if (numday==7)
+{
+    Console.WriteLine ("Ура это выходной");
+}
+else if (numday==6) {
+    Console.WriteLine ("Ура это выходной");
+}
+if (numday < 1 || numday >7) 
+{
+    Console.WriteLine ("Такого дня нет");
+    
+}
+    
