bc2ea6c05aa22c1885a7ae3574ff9801c58a739a
VeGu <VeGu@bk.ru>
2023-08-05 18:16:46 +0700

    Является ли число палиндромом

---
+++ DZ8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число ");
+string num = Console.ReadLine();
+int number = int.Parse(num);
+int TenThousandPart = number/10000;
+int LastPart = number%10;
+int ThousandPart = (number/1000)%10;
+int TenPart = (number%100)/10;
+if (TenThousandPart == LastPart && ThousandPart == TenPart) {
+    Console.WriteLine($"Ваше число {num} являеться палиндромом");
+}
+else {
+    Console.WriteLine($"Ваше число {num} не являеться палиндромом");
+}
