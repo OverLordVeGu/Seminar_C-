ae85ddb02a7ba8edc2389b6154c07e22b4c3fe67
VeGu <VeGu@bk.ru>
2023-08-05 23:03:27 +0700

    Кубический ряд числа N

---
+++ DZ10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Enter the number MAN - ");
+int Num = int.Parse(Console.ReadLine());
+int start = 1;
+while (start <= Num) {
+    double b = Math.Pow (start, 3);
+    Console.Write($"{b} ");
+    start++;
+}
