83e3c2b0bc5e39a7249f1cfef36ff411dad0c3db
VeGu <VeGu@bk.ru>
2023-08-06 22:23:41 +0700

    Цикл с возведением числа №1 в степень числа №2

---
+++ DZ11/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear ();
+Console.Write ("Введи число №1: ");
+int num1 = int.Parse (Console.ReadLine ());
+Console.Write ("Введи число №2: ");
+int num2 = int.Parse (Console.ReadLine ());
+
+// double Stepen (int num1, int num2) {
+//     double result = Math.Pow (num1, num2);
+//     return result;
+// }
+
+double Stepen (int num1, int num2) {
+    int result = num1;
+    for (int i = 1; i < num2; i++) {
+        result *= num1;
+    }
+    return result;
+}
+
+double result = Stepen (num1, num2);
+Console.Write ($"Если число {num1} возвести в {num2} результат = {result}");
+
+
