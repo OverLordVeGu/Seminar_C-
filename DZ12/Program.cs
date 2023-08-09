2af0d6cd537d363ab384ca4aa8fd07ff2fb03886
VeGu <VeGu@bk.ru>
2023-08-09 21:22:06 +0700

    Сумма всех цифр из которых состоит число

---
+++ DZ12/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear ();
+int EnterNumber ()
+{
+    Console.Write ("Введите число ");
+    int number = int.Parse (Console.ReadLine ());
+    return number;
+}
+
+int Sum (int number)
+{
+    int result = 0;
+        for (int i = 0; i < 100; i++)
+    {
+        int OneNumber = number%10;
+        number = number/10;
+        result += OneNumber;
+    }
+    return result;
+}
+int StrNumbers = EnterNumber ();
+int GetSum = Sum (StrNumbers);
+Console.WriteLine (GetSum);
+
