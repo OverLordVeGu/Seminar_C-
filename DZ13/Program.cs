585e0de60c6ca06a6c771a6ead84fdbf076f7372
VeGu <VeGu@bk.ru>
2023-08-12 15:29:21 +0700

    Вывод 8-ми значного массива

---
+++ DZ13/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+int [] FillArray ()
+{
+    int [] array = new int [8];
+    Random random = new Random ();
+    for (int i = 0; i < 8; i++) 
+    {
+        array [i] = random.Next (1, 100);
+    }
+    return array;
+}
+
+void PrintArray (int [] array)
+{
+    Console.Write ("[");
+    for (int i = 0; i < 7; i++)
+    {
+        Console.Write ($"{array [i]}, ");
+    }
+    Console.Write ($"{array[7]}");
+    Console.WriteLine ("]");
+}
+
+int [] array = FillArray ();
+PrintArray (array);
